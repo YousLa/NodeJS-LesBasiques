03472125b5529df882b9a48ec974c7038e9c819b
Yousra MSI <eljilaliyousra@gmail.com>
2024-01-15 10:08:00 +0100

    Serveur Fichier Web

---
+++ ServeurFichiersWeb/Apps.js
@@ -0,0 +1,10 @@
+// ! Exercice : Programmer app.js (Npde) qui fait serveur de fichiers (web) avec express
+let express = require('express');
+let app = express();
+// dirname reprends tout le chemin vers le dossier parent de notre fichier App.js
+// console.log(__dirname);
+// On concatène htdocs pour signifié que c'est la racine de notre projet
+app.use('/', express.static(__dirname + "/htdocs"));
+app.listen(8080, function () {
+    console.log('Listening on port 8080');
+});
