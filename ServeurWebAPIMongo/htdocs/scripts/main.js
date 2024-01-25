55abe7980c439642fe12434c40ed39c8c7b95e6f
Yousra MSI <eljilaliyousra@gmail.com>
2024-01-25 10:26:35 +0100

    ServeurWebApiMongo

--- ServeurWebAPIMongo/htdocs/scripts/main.js
+++ ServeurWebAPIMongo/htdocs/scripts/main.js
@@ -2,8 +2,15 @@ fetch('http://127.0.0.1:8000/movies')
     .then(function (response) {
         return response.json();
         })
-        .then(function (data) { 
-            console.log(data);
+        .then(function (movies) { 
+            movies.forEach(movie => {
+                const para = document.createElement("li");
+                const node = document.createTextNode(movie.Series_Title);
+                para.appendChild(node);
+                const element = document.getElementById("movies");
+                element.appendChild(para);
+            });
+            
         })
     .catch(function (err) { 
         console.log("Something went wrong!", err);
